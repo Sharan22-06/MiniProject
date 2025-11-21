213591aded9ff413201aa7133da8337ad5c0aa9f
luffy00069 <rizzlerluffy69@gmail.com>
2025-11-21 15:06:29 +0530

    second Commit

--- src - backup/hooks/useBottomSpacing.ts
+++
@@ -1,9 +0,0 @@
-import { useSafeAreaInsets } from "react-native-safe-area-context"
-import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs"
-
-export default function useBottomSpacing(extra = 8) {
-  const insets = useSafeAreaInsets()
-  // useBottomTabBarHeight returns 0 when not inside a tab navigator
-  const tabBarHeight = useBottomTabBarHeight ? useBottomTabBarHeight() : 0
-  return (insets?.bottom || 0) + (tabBarHeight || 0) + extra
-}
